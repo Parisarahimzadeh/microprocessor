905e7ed2192069f42ac78a1d78e4461d50dfa341
Parisarahimzadeh <106354887+Parisarahimzadeh@users.noreply.github.com>
2023-12-12 18:25:10 +0330

    trafficLight
    ATMega16

---
+++ TrafficLight/CodeTrafficLight/trafficLight.c
@@ -0,0 +1,66 @@
+/*
+ * trafficLight.c
+ *
+ * Created: 12/9/2023 9:23:28 PM
+ * Author: Parisa
+ */
+
+#include <Mega16.h>
+#include <delay.h>
+
+
+void main(void)
+{
+DDRA = 0xFF; 
+while (1)
+    {
+        PORTA.0 = 0;
+        PORTA.1 = 0;
+        PORTA.2 = 1; 
+        PORTA.3 = 1;
+        PORTA.4 = 0;
+        PORTA.5 = 0;
+        delay_ms(1000);
+        
+        PORTA.0 = 0;
+        PORTA.1 = 1;
+        PORTA.2 = 0; 
+        PORTA.3 = 1;
+        PORTA.4 = 0;
+        PORTA.5 = 0;
+        delay_ms(1000);
+        
+        PORTA.0 = 1;
+        PORTA.1 = 0;
+        PORTA.2 = 0; 
+        PORTA.3 = 1;
+        PORTA.4 = 0;
+        PORTA.5 = 0;
+        delay_ms(1000); 
+        
+        PORTA.0 = 1;
+        PORTA.1 = 0;
+        PORTA.2 = 0; 
+        PORTA.3 = 0;
+        PORTA.4 = 0;
+        PORTA.5 = 1;
+        delay_ms(1000);
+        
+        PORTA.0 = 1;
+        PORTA.1 = 0;
+        PORTA.2 = 0; 
+        PORTA.3 = 0;
+        PORTA.4 = 1;
+        PORTA.5 = 0;
+        delay_ms(1000); 
+        
+        PORTA.0 = 1;
+        PORTA.1 = 0;
+        PORTA.2 = 0; 
+        PORTA.3 = 0;
+        PORTA.4 = 0;
+        PORTA.5 = 1;
+        delay_ms(1000);
+
+    }
+}
